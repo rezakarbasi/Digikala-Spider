5de84193b148dbc2868caca6d15758692b895ee5
Reza Karbasi <arzkarbasi@gmail.com>
2020-02-10 23:42:56 +0330

    digiSpider updated

--- myDigikalaCrawler/ListMaker/ListMaker/spiders/digiSpider.py
+++ myDigikalaCrawler/ListMaker/ListMaker/spiders/digiSpider.py
@@ -1,11 +1,45 @@
-# -*- coding: utf-8 -*-
 import scrapy
+from ..items import ListmakerItem
+
+Max_List = 2
+
+# the first url is https://www.digikala.com/search/category-mobile-phone/?pageno=1&sortby=4
+firstUrl = 'https://www.digikala.com/search/category-mobile-phone/?pageno='
+secondUrl = '&sortby=4'
 
 
 class DigispiderSpider(scrapy.Spider):
     name = 'digiSpider'
-    allowed_domains = ['https://www.digikala.com/search/category-mobile-phone/']
-    start_urls = ['http://https://www.digikala.com/search/category-mobile-phone//']
+    start_urls = [firstUrl+str(1)+secondUrl]
+
+    def __init__(self):
+        self.pageNum = 1
+        self.inPage=1
 
     def parse(self, response):
+
+        print('################################new-page################################')
+        prods = response.css(".is-plp")
+
+        i = 0
+        for prod in prods:
+            print()
+            i += 1
+            print(i)
+            link = prod.css(".c-product-box__title a::attr(href)").get()
+            yield response.follow(link, callback=self.parsePage)
+
+        if self.pageNum < Max_List:
+            self.pageNum += 1
+            newUrl = firstUrl+str(self.pageNum)+secondUrl
+            yield response.follow(newUrl, callback=self.parse)
+
+    def parsePage(self, response):
+        # items = TutorialItem()
+        name = response.css('.c-product__title::text').extract()[0]
+        print()
+        print('-----------------------new-page-----------------------')
+        print(self.inPage)
+        self.inPage+=1
+        print(name)
         pass
--- myDigikalaCrawler/crawler.py
+++ myDigikalaCrawler/crawler.py
@@ -3,84 +3,85 @@ import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.keys import Keys
 from time import sleep
 
-maxTry = 30
+def crawl_a_page(url):
+    maxTry = 30
 
-browser = webdriver.Chrome('/usr/bin/chromedriver')
-browser.get('https://www.digikala.com/product/dkp-2117141')
-sleep(1)
-browser.find_element_by_css_selector('.c-product__engagement-link').click()
+    browser = webdriver.Chrome('/usr/bin/chromedriver')
+    browser.get(url)
+    sleep(1)
+    browser.find_element_by_css_selector('.c-product__engagement-link').click()
 
-page = 1
+    page = 1
 
-sleep(3)
+    sleep(3)
 
-endPage = int(browser.find_element_by_css_selector(
-    '.c-pager ul.c-pager__items > li:last-child a').get_attribute('data-page'))
-n = 0
-tries = 0
-
-while page <= endPage:
+    endPage = int(browser.find_element_by_css_selector(
+        '.c-pager ul.c-pager__items > li:last-child a').get_attribute('data-page'))
+    n = 0
     tries = 0
-    print('-------------------------page'+str(page) +
-          '-------------------------')
-    sleep(2)
-    try:
-        if tries > maxTry:
-            break
 
-        for i in range(1, 16):
-            n += 1
-            st = '#product-comment-list ul.c-comments__list > li:nth-child('+str(
-                i)+')'
-            comment = browser.find_element_by_css_selector(
-                st+' .article p').text
-            data_person = browser.find_element_by_css_selector(
-                st+' .aside .c-comments__user-shopping').text
-            likes = browser.find_element_by_css_selector(
-                st+' .article .js-comment-like').get_attribute('data-counter')
-            dislikes = browser.find_element_by_css_selector(
-                st+' .article .js-comment-dislike').get_attribute('data-counter')
+    while page <= endPage:
+        tries = 0
+        print('-------------------------page'+str(page) +
+            '-------------------------')
+        sleep(2)
+        try:
+            if tries > maxTry:
+                break
 
-            print(comment)
-            print(data_person)
-            print(likes)
-            print(dislikes)
-            print('\n\n')
+            for i in range(1, 16):
+                n += 1
+                st = '#product-comment-list ul.c-comments__list > li:nth-child('+str(
+                    i)+')'
+                comment = browser.find_element_by_css_selector(
+                    st+' .article p').text
+                data_person = browser.find_element_by_css_selector(
+                    st+' .aside .c-comments__user-shopping').text
+                likes = browser.find_element_by_css_selector(
+                    st+' .article .js-comment-like').get_attribute('data-counter')
+                dislikes = browser.find_element_by_css_selector(
+                    st+' .article .js-comment-dislike').get_attribute('data-counter')
 
-    except:
-        tries += 1
+                print(comment)
+                print(data_person)
+                print(likes)
+                print(dislikes)
+                print('\n\n')
 
-        print('dastan shod')
-        pass
+        except:
+            tries += 1
 
-    page += 1
-    pages = browser.find_element_by_css_selector('.c-pager__items')
-    j = 2
+            print('dastan shod')
+            pass
 
-    while True:
-        nexPage = pages.find_element_by_css_selector(
-            'ul > li:nth-child('+str(j) + ') a')
-        print(page == int(nexPage.get_attribute('data-page')))
-        print(int(nexPage.get_attribute('data-page')))
-        if page == int(nexPage.get_attribute('data-page')):
-            print('-----------------')
-            while True:
-                try:
-                    if tries > maxTry:
+        page += 1
+        pages = browser.find_element_by_css_selector('.c-pager__items')
+        j = 2
+
+        while True:
+            nexPage = pages.find_element_by_css_selector(
+                'ul > li:nth-child('+str(j) + ') a')
+            print(page == int(nexPage.get_attribute('data-page')))
+            print(int(nexPage.get_attribute('data-page')))
+            if page == int(nexPage.get_attribute('data-page')):
+                print('-----------------')
+                while True:
+                    try:
+                        if tries > maxTry:
+                            break
+
+                        nexPage.click()
                         break
+                    except:
+                        tries += 1
 
-                    nexPage.click()
-                    break
-                except:
-                    tries += 1
-
-                    print('-------------------')
-                    sleep(2)
-                    pass
-            print('moved')
-            break
-        j += 1
+                        print('-------------------')
+                        sleep(2)
+                        pass
+                print('moved')
+                break
+            j += 1
 
 
-browser.close()
-print(n)
+    browser.close()
+    print(n)
