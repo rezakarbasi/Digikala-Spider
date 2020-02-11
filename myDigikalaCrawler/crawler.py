9c0133f35afe15d63fdc8bb1110957437a9ce749
Reza Karbasi <arzkarbasi@gmail.com>
2020-02-11 23:22:27 +0330

    crawler completed . but it's very slow

--- myDigikalaCrawler/crawler.py
+++ myDigikalaCrawler/crawler.py
@@ -4,76 +4,136 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import pandas as pd
 
-def crawl_a_page(browser, url):
+
+def crawl_a_page(browser, url, endPage=0):
     maxTry = 30
+    tries = 0
 
     browser.get(url)
-    sleep(1)
-    browser.find_element_by_css_selector('.c-product__engagement-link').click()
+    # sleep(2)
+    # browser.find_element_by_css_selector('.c-product__engagement-link').click()
+    while True:
+        try:
+            if tries > maxTry:
+                return
+
+            browser.find_elements_by_css_selector(
+                '.c-box-tabs__tab')[2].click()
+            break
+        except:
+            tries += 1
+            pass
 
     page = 1
 
     sleep(3)
 
-    endPage = int(browser.find_element_by_css_selector(
+    if len(browser.find_elements_by_css_selector('.c-pager ul.c-pager__items > li')) == 0:
+        return
+    maxCommentPage = int(browser.find_element_by_css_selector(
         '.c-pager ul.c-pager__items > li:last-child a').get_attribute('data-page'))
+
+    if endPage == 0:
+        endPage = maxCommentPage
+    endPage = min(endPage, maxCommentPage)
+
     n = 0
     tries = 0
+
+    name = browser.find_element_by_css_selector('.c-product__title').text
+    
+    try:
+        price = browser.find_element_by_css_selector('.js-price-value').text
+    except:
+        price=0
 
     while page <= endPage:
         tries = 0
         sleep(2)
         try:
             if tries > maxTry:
-                break
+                return
 
-            for i in range(1, 16):
+            for comment_bar in browser.find_elements_by_css_selector('#product-comment-list ul.c-comments__list > li'):
                 n += 1
-                st = '#product-comment-list ul.c-comments__list > li:nth-child('+str(
-                    i)+')'
-                comment = browser.find_element_by_css_selector(
-                    st+' .article p').text
-                data_person = browser.find_element_by_css_selector(
-                    st+' .aside .c-comments__user-shopping').text
-                likes = browser.find_element_by_css_selector(
-                    st+' .article .js-comment-like').get_attribute('data-counter')
-                dislikes = browser.find_element_by_css_selector(
-                    st+' .article .js-comment-dislike').get_attribute('data-counter')
 
-                yield comment, data_person, likes, dislikes
+                comment = comment_bar.find_element_by_css_selector(
+                    '.article p').text
+                data_person = comment_bar.find_element_by_css_selector(
+                    '.aside .c-comments__user-shopping').text
+                likes = comment_bar.find_element_by_css_selector(
+                    '.article .js-comment-like').get_attribute('data-counter')
+                dislikes = comment_bar.find_element_by_css_selector(
+                    '.article .js-comment-dislike').get_attribute('data-counter')
+
+                yield name, price, comment, data_person, likes, dislikes
 
         except:
             tries += 1
             pass
 
         page += 1
+        if page > endPage:
+            break
+        
         pages = browser.find_elements_by_css_selector('.c-pager__items li')
         j = 1
-        
-        temp=0
+
+        temp = 0
         while True:
             nexPage = pages[j].find_element_by_css_selector('a')
             if page == int(nexPage.get_attribute('data-page')):
                 while True:
                     try:
                         if tries > maxTry:
-                            break
+                            return
 
                         nexPage.click()
-                        temp=1
+                        temp = 1
                         break
                     except:
                         tries += 1
 
-                        sleep(1)
+                        sleep(3)
                         pass
                 break
             j += 1
-            if temp>0:
+            if temp > 0:
                 break
 
 
-# if __name__ == "__main__":
+numPagesList = 3
+
 browser = webdriver.Chrome('/usr/bin/chromedriver')
-url = 'https://www.digikala.com/product/dkp-2117141/%DA%AF%D9%88%D8%B4%DB%8C-%D9%85%D9%88%D8%A8%D8%A7%DB%8C%D9%84-%D8%B4%DB%8C%D8%A7%D8%A6%D9%88%D9%85%DB%8C-%D9%85%D8%AF%D9%84-redmi-note-8-m1908c3jg-%D8%AF%D9%88-%D8%B3%DB%8C%D9%85-%DA%A9%D8%A7%D8%B1%D8%AA-%D8%B8%D8%B1%D9%81%DB%8C%D8%AA-64-%DA%AF%DB%8C%DA%AF%D8%A7%D8%A8%D8%A7%DB%8C%D8%AA'
-    
+
+# for a in crawl_a_page(browser, 'https://www.digikala.com/product/dkp-2117141/'):
+#     print(a)
+
+firstUrl = 'https://www.digikala.com/search/category-mobile-phone/?pageno='
+secondUrl = '&sortby=4'
+
+prodUrls = []
+
+for i in range(numPagesList):
+    url = firstUrl+str(i+1)+secondUrl
+    browser.get(url)
+
+    for element in browser.find_elements_by_css_selector('.c-product-box__content--row .c-product-box__title'):
+        prodUrls += [element.find_element_by_css_selector(
+            'a').get_attribute('href')]
+
+saveData = {'name': [], 'price': [], 'comment': [],
+            'data_person': [], 'likes': [], 'dislikes': []}
+pd.DataFrame(saveData).to_csv('crawled2.csv', header=True)
+
+for url in prodUrls:
+    saveData = {'name': [], 'price': [], 'comment': [],
+                'data_person': [], 'likes': [], 'dislikes': []}
+    for name, price, comment, data_person, likes, dislikes in crawl_a_page(browser, url, 10):
+        saveData['name'] += [name]
+        saveData['price'] += [price]
+        saveData['comment'] += [comment]
+        saveData['data_person'] += [data_person]
+        saveData['likes'] += [likes]
+        saveData['dislikes'] += [dislikes]
+    pd.DataFrame(saveData).to_csv('crawled2.csv', header=False, mode='a')
